2c1709844d8a04eca70fb9f7ca91b9b230018af2
yencoding <79498424+yeni28@users.noreply.github.com>
2025-08-21 19:51:02 +0900

    [FEAT]:헬스 라우트 추가

---
+++ app/api/_health/route.js
@@ -0,0 +1,5 @@
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+export async function GET() {
+  return new Response('ok', { status: 200, headers: { 'content-type': 'text/plain' } });
+}
